c35a03f354462b944f505091928b2913276b039c
Roy Hyunjin Han <rhh@crosscompute.com>
2017-10-01 13:04:32 -0400

    Replace string with boolean

--- get-timestamps.py
+++ get-timestamps.py
@@ -4,8 +4,8 @@ from argparse import ArgumentParser
 from datetime import datetime, timedelta
 
 
-def get_timestamps(interval_in_minutes=30, method='ceil'):
-    method = getattr(math, args.method)
+def get_timestamps(interval_in_minutes=30, with_next=False):
+    method = getattr(math, 'ceil' if with_next else 'floor')
 
     now = datetime.now()
     old_minute_count = now.minute
@@ -21,11 +21,11 @@ if __name__ == '__main__':
     argument_parser.add_argument(
         '-i', '--interval_in_minutes', type=int, default=30)
     argument_parser.add_argument(
-        '-m', '--method', default='floor')
+        '-n', '--next', action='store_true')
     args = argument_parser.parse_args()
 
     timestamp1, timestamp2 = get_timestamps(
-        args.interval_in_minutes, args.method)
+        args.interval_in_minutes, args.next)
     timestamp_format = '%Y%m%d-%H%M'
     print('%s - %s: %s minutes' % (
         timestamp1.strftime(timestamp_format),
