26d0ffa17a0bd3458868a59a1f566310f3bd3958
Roy Hyunjin Han <rhh@crosscompute.com>
2021-07-16 08:52:43 -0400

    Copy to clipboard

--- make-password.py
+++ make-password.py
@@ -27,8 +27,16 @@ def get_alphabet(code):
 
 if __name__ == '__main__':
     argument_parser = ArgumentParser()
-    argument_parser.add_argument('--length', '-l', metavar='32', type=int, default=32)
-    argument_parser.add_argument('--code', '-c', metavar='dlsp', default='dl')
+    argument_parser.add_argument(
+        '--length', '-l', metavar='32', type=int, default=32)
+    argument_parser.add_argument(
+        '--code', '-c', metavar='dlsp', default='dl')
     args = argument_parser.parse_args()
     alphabet = get_alphabet(args.code)
-    print(make_random_string(alphabet, args.length))
+    random_string = make_random_string(alphabet, args.length)
+    print(random_string)
+    try:
+        import pyperclip
+        pyperclip.copy(random_string)
+    except Exception:
+        print('pip install -U pyperclip')
